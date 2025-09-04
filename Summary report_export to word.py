5dda7b1e059e90e88a4b5bc29873348556083238
Elise <elise.peng.uin07@gmail.com>
2025-09-04 16:13:54 +0800

    Update Summary report_export to word.py

--- Summary report_export to word.py
+++ Summary report_export to word.py
@@ -14,7 +14,8 @@ Title = st.text_input("Please enter the file name to export.")
 Name = st.text_input("Please enter the document editor's name.")
 Project = st.text_input("Please enter the project name")
 Date= st.text_input("Please enter the edit date")
-Test = st.selectbox("Please select the test type", ["Software Integration Test", "Software Qualification Test", "System Integration Test", "System Qualification Test"])
+Test_en = st.selectbox("Please select the test type(EN)", ["Software Integration Test", "Software Qualification Test", "System Integration Test", "System Qualification Test"])
+Test_cn = st.selectbox("Please select the test type"(CN), ["軟體整合測試", "軟體合格測試", "系統整合測試", "系統合格測試"])
 Test_version = st.text_input("Please enter the text version")
 
 #Upload file
@@ -40,33 +41,35 @@ if uploaded_file2 is not None:
 
 #Purpose
 st.header("1. Purpose / 目的")
-if Project and Test:
-  st.write(f"""The purpose of this document is the {Test} summary report of the {Project} project.\n
-本文件目的為{Project}項目的{Test}總結報告""")
+if Project and Test_en and Test_cn:
+  st.write(f"""The purpose of this document is the {Test_en} summary report of the {Project} project.\n
+本文件目的為{Project}項目的{Test_cn}總結報告""")
 else:
    st.info("Please fill in project name and test type.")
 
 #Scope
 st.header("2. Scope / 適用範圍")
-if Project and Test and Title:
-  st.write(f"""The scope of the software qualification test summary report of the {Project} project is applicable to the {Test}ing performed on the software version defined in the release plan.\n
-{Project} 項目專案的{Title}的範圍適用於發布計劃裡定義的軟體版本所進行的{Test}
+if Project and Test_en and Test_cn and Title:
+  st.write(f"""The scope of the software qualification test summary report of the {Project} project is applicable to the {Test_en}ing performed on the software version defined in the release plan.\n
+{Project} 項目專案的{Title}的範圍適用於發布計劃裡定義的軟體版本所進行的{Test_cn}
 """)
 else:
   st.info("Please fill in project name, test type and file name.")
 
 #Overview
-st.header(f"3. {Test} Summary Report Overview / {Test}總結報告概述")
+st.header(f"3. {Test_en} Summary Report Overview / {Test_cn}總結報告概述")
 st.subheader("3.1 Test Information Overview / 測試資訊概述 ")
-if Test and Test_version:
-  st.write(f"""This information is for {Test_version} version to fully test the {Test}.\n此資訊為{Test_version}版本進行的{Test} (全量)""")
+if Test_en and Test_cn and Test_version:
+  st.write(f"""This information is for {Test_version} version to fully test the {Test_en}.\n此資訊為{Test_version}版本進行的{Test_cn} (全量)""")
 else:
   st.info("Please fill in Test type and Test version")
 
 st.subheader("3.2 Overall Status / 總體狀態")
 st.makedown("3.2.1Progress And Criteria / 進度和標準")
-if Project:
-  st.wrtie(f"""This information is for {Test_version} version to fully test the software qualification testing.\n此資訊為{Test_version}版本進行的軟體合格測試 (全量)""")
+if Project and Test_en and Test_cn:
+  st.wrtie(f"""The table below is the progress of {Test_en} for {Project}.\n下表是{Project}項目的{Text_cn}的進度和標準。""")
+else:
+   st.info("Please fill in Test type and Project")
 
 
 
